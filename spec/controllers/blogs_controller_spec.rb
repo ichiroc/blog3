713c2a782502cbdfbb1bb6ad19c59a117d268905
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-27 19:19:04 +0900

    不要な describe を削除

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -3,23 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe BlogsController do
+
   fixtures(:all)
 
-  describe BlogsController do
-    describe 'GET #index' do
-      it '@blogsに全てのBlogが入っていること' do
-        get :index
-        @blogs = assigns(:blogs)
-        expect(@blogs.size).to be 2
-      end
+  describe 'GET #index' do
+    it '@blogsに全てのBlogが入っていること' do
+      get :index
+      @blogs = assigns(:blogs)
+      expect(@blogs.size).to be 2
     end
+  end
 
-    describe 'POST #create' do
-      it '新規作成後に@blogのshowに遷移すること' do
-        post :create, params: { blog: { title: 'hoge' } }
-        @blog = assigns(:blog)
-        expect(response).to redirect_to blog_path(@blog)
-      end
+  describe 'POST #create' do
+    it '新規作成後に@blogのshowに遷移すること' do
+      post :create, params: { blog: { title: 'hoge' } }
+      @blog = assigns(:blog)
+      expect(response).to redirect_to blog_path(@blog)
     end
   end
 end
