66a294d1dcce46764ec5469333b465b0fc7841ba
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-27 19:06:18 +0900

    rubocop

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -1,12 +1,13 @@
 # coding: utf-8
-require "rails_helper"
+# frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe BlogsController do
   fixtures(:all)
 
   describe BlogsController do
     describe 'GET #index' do
-      it "@blogsに全てのBlogが入っていること" do
+      it '@blogsに全てのBlogが入っていること' do
         get :index
         @blogs = assigns(:blogs)
         expect(@blogs.size).to be 2
@@ -14,12 +15,11 @@ RSpec.describe BlogsController do
     end
 
     describe 'POST #create' do
-      it "新規作成後に@blogのshowに遷移すること" do
-        post :create, params: { blog: { title: 'hoge' }}
+      it '新規作成後に@blogのshowに遷移すること' do
+        post :create, params: { blog: { title: 'hoge' } }
         @blog = assigns(:blog)
         expect(response).to redirect_to blog_path(@blog)
       end
     end
-
   end
 end
