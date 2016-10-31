b50ced27311657b1f999fe5611af56e4380390e6
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-31 09:02:05 +0900

    リファクタリングí ½í²

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -4,25 +4,25 @@ require 'rails_helper'
 
 RSpec.describe EntriesController do
   fixtures(:all)
+
+  let(:blog) { Blog.first }
+  let(:first_entry) { blog.entries.first }
+
   describe 'GET #show' do
     it '@entry に entry が入っていること' do
-      blog = Blog.first
-      entry = blog.entries.first
-      get :show, params: { blog_id: blog, id: entry }
-      expect(assigns(:entry)).to eq entry
+      get :show, params: { blog_id: blog, id: first_entry }
+      expect(assigns(:entry)).to eq first_entry
     end
 
     it '親子関係が一致しない URL に移動したらリダイレクトされること' do
-      blog = Blog.first
-      other_entry = entries(:other_blog)
-      get :show, params: { blog_id: blog, id: other_entry }
+      other_blog_entry = entries(:other_blog)
+      get :show, params: { blog_id: blog, id: other_blog_entry }
       expect(response).to redirect_to blog
     end
   end
 
   describe 'GET #new' do
-    it '@entry に Entry クラスが入っていること' do
-      blog = Blog.first
+    it '@entry に Entry のインスタンスが入っていること' do
       get :new, params: { blog_id: blog }
       entry = assigns(:entry)
       expect(entry.class).to eq Entry
@@ -31,24 +31,19 @@ RSpec.describe EntriesController do
   end
 
   describe 'GET #edit' do
-    it '@entry に Entry クラスが入っていること' do
-      blog = Blog.first
-      entry = blog.entries.first
-      get :edit, params: { blog_id: blog, id: entry }
-      entry = assigns(:entry)
-      expect(entry.class).to eq Entry
+    it '@entry に Entry のインスタンスが入っていること' do
+      get :edit, params: { blog_id: blog, id: first_entry }
+      expect(assigns(:entry).class).to eq Entry
     end
   end
 
   describe 'POST #create' do
     it 'POST すると記事がの数が増えること' do
-      blog = Blog.first
       entry_params = { title: 'hoge', body: 'fuga' }
       expect { post :create, params: { blog_id: blog, entry: entry_params } }.to change { Entry.count }.by(+1)
     end
 
     it '作成後にその記事にリダイレクトされること' do
-      blog = Blog.first
       post :create, params: { blog_id: blog, entry: { title: 'hoge', body: 'fuga' } }
       expect(response).to redirect_to([blog, Entry.last])
     end
@@ -56,8 +51,7 @@ RSpec.describe EntriesController do
 
   describe 'PATCH #update' do
     before do
-      blog = Blog.first
-      @entry = blog.entries.first
+      @entry = first_entry
       expect(@entry.title).to_not be 'hoge'
       patch :update, params: { blog_id: 1, id: @entry, entry: { title: 'hoge', body: @entry.body } }
       @entry.reload
