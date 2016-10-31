812dae28d77e585cb0d2af489af8897d0e68af26
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-31 11:18:53 +0900

    Blog の作成画面を表示する処理を before に移した

--- spec/features/blogs_spec.rb
+++ spec/features/blogs_spec.rb
@@ -3,17 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Blog do
-  scenario 'Blogの新規作成時にtitleを入力しなかった場合にエラーが表示されること' do
+  before do
+    # 最初にブログ作成画面を表示しておく
     visit blogs_path
     click_link 'New Blog'
+  end
+
+  scenario 'Blogの新規作成時にtitleを入力しなかった場合にエラーが表示されること' do
     click_button 'Submit'
     expect(page).to have_content "Title can't be blank"
   end
 
   scenario 'Blogの新規作成時にtitleを入力した場合はデータが保存され閲覧画面に遷移すること' do
-    visit blogs_path
-    click_link 'New Blog'
     fill_in 'Title', with: 'hoge'
     expect { click_button 'Submit' }.to change(Blog, :count).by(+1)
+    expect(current_path).to eq blog_path(Blog.last)
+    expect(page).to have_content 'Successfully blog created.'
   end
 end
