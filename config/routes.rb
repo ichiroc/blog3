16c8cfc2660c63505caa4069a287d84270d6bd00
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-11-02 06:26:20 +0900

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  root 'blog#index'
+  root 'blogs#index'
   resources :blogs do
     resources :entries, except: [:index] do
       resources :comments, only: [:new, :create, :approve, :destroy] do
